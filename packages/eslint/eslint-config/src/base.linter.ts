960512f13d4b7e96952762af7254f0322fe76d00
Nace Logar <the.code.destroyer@gmail.com>
2025-05-31 22:08:13 +0200

    feat(eslint): Add support to ignore .gitignore entries

--- packages/eslint/eslint-config/src/base.linter.ts
+++ packages/eslint/eslint-config/src/base.linter.ts
@@ -1,10 +1,26 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+import { includeIgnoreFile } from '@eslint/compat';
 import eslintPlugin from '@eslint/js';
 import type { Linter } from 'eslint';
 import { defineConfig } from 'eslint/config';
 
 import { baseRules } from '#base.rules';
 
+const cwdURL = `file://${process.cwd()}/`;
+const gitignorePath = fileURLToPath(new URL(`.gitignore`, cwdURL));
+
+const fileExists = fs.existsSync(gitignorePath);
+
+let gitignoreConfig = {};
+
+if (fileExists) {
+  gitignoreConfig = includeIgnoreFile(gitignorePath);
+}
+
 export const baseConfig: Linter.Config[] = defineConfig([
+  gitignoreConfig,
   {
     name: '@tcd-devkit/eslint-config',
     extends: [eslintPlugin.configs.recommended],
