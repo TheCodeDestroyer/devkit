58073a30797b96d46fa51b55be850c2578098922
Nace Logar <the.code.destroyer@gmail.com>
2025-11-09 19:51:07 +0100

    fix(eslint-config-next): Add missing Node globals

---
+++ .changeset/deep-snakes-win.md
@@ -0,0 +1,5 @@
+---
+'@tcd-devkit/eslint-config-next': patch
+---
+
+Add missing Node globals
--- packages/eslint/eslint-config-next/src/next.linter.ts
+++ packages/eslint/eslint-config-next/src/next.linter.ts
@@ -1,10 +1,16 @@
 import eslintPluginNext from '@next/eslint-plugin-next';
 import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 const nextCoreWebVitals = eslintPluginNext.configs['core-web-vitals'];
 
 export const nextConfig = defineConfig({
   name: '@tcd-devkit/eslint-config-next',
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
   extends: [nextCoreWebVitals],
   files: ['**/*.{js,jsx,ts,tsx,mjs,cjs}'],
 });
